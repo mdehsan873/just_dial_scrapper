6340fa92a84b7ddd590f0d8166f79d963d87972f
monu <mdehsan873@gmail.com>
2022-01-10 22:08:44 +0530

    code for review

--- constant.py
+++
@@ -1,9 +0,0 @@
-#class name for electricain name
-nameclass='jcn'
-#class name for electricain info
-contactclass='contact-info'
-#class name for electricain add
-addressclass='mrehover'
-#Scrapped Url
-url="https://www.justdial.com/Mumbai/Electricians"
-
